2e70816a38295f4b0c1b7fe109e67a44350cdcd3
SteamBlizzard <dan.sabo@live.ca>
2024-09-30 21:58:34 -0600

    Add comments.

--- 1_music.s
+++ 1_music.s
@@ -89,31 +89,34 @@ nextstmt                ; next BASIC statement
     lda #255        ; load timer 1 as 255
     sta TIMER2      ; store into timer 2 register
 
-    lda #31       ; load timer 3 as 31
+    lda #31         ; load timer 3 as 31
     sta TIMER3      ; store into timer 3 register
 
     lda #Fs1        ; load x register as F#1
     sta SPKR3       ; store into high frequency register
 
 gameloop:           ; play a note on loop
-    lda TIMER3
-    beq volume_on
-    sbc #16
-    beq volume_off
-    jmp next_loop
+    lda TIMER3      ; check if timer 3 is 0
+    beq volume_on   ; if so, turn volume on
+    sbc #16         ; check if timer 3 is 16
+    beq volume_off  ; if so, turn volume off
+    jmp next_loop   ; otherwise, continue loop
 
 volume_off:
-    lda #0
-    sta VOL
-    jmp next_loop
+    lda #0          ; set volume to 0
+    sta VOL         ; store into volume register
+    jmp next_loop   ; continue loop
 
-volume_on:
-    lda #5
-    sta VOL
+volume_on:          ; play note
+    lda #5          ; set volume to 5
+    sta VOL         ; store into volume register
 
 next_loop:
-    jsr increment_timer
-    jmp gameloop
+    jsr increment_timer ; increment the timer
+    jmp gameloop        ; loop
+
+end:                    ; end of program
+    rts                 ; return from subroutine
 
 ; -------- END PROGRAM --------
 
@@ -125,35 +128,30 @@ next_loop:
     ; decrements the timers and loops them
     ; allows for approx. 16 seconds of looped data, such as music
 increment_timer:
-    lda TIMER1
-    beq increment_timer2
-    dec TIMER1
-    rts
+    lda TIMER1              ; check if timer 1 is 0
+    beq increment_timer2    ; if so, increment timer 2
+    dec TIMER1              ; otherwise, decrement timer 1
+    rts                     ; return from subroutine
 
 increment_timer2:
-    adc #255
-    sta TIMER1
-    lda TIMER2
-    beq increment_timer3
-    dec TIMER2
-    rts
+    adc #255                ; reset timer 1
+    sta TIMER1              ; store into timer 1 register
+    lda TIMER2              ; check if timer 2 is 0
+    beq increment_timer3    ; if so, increment timer 3
+    dec TIMER2              ; otherwise, decrement timer 2
+    rts                     ; return from subroutine
 
 increment_timer3:
-    adc #255
-    sta TIMER2
-    lda TIMER3
-    beq loop_timer
-    dec TIMER3
-    rts
+    adc #255                ; reset timer 2
+    sta TIMER2              ; store into timer 2 register
+    lda TIMER3              ; check if timer 3 is 0
+    beq loop_timer          ; if so, loop timer
+    dec TIMER3              ; otherwise, decrement timer 3
+    rts                     ; return from subroutine
 
 loop_timer:
-    adc #31
-    sta TIMER3
-    rts
+    adc #31                 ; reset timer 3
+    sta TIMER3              ; store into timer 3 register
+    rts                     ; return from subroutine
 
 ; END increment_timer
-
-; end
-    ; end of program
-end:
-    rts             ; return from subroutine

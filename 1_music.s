1388945de8687d212fb74f1e9da4a42580028dea
SteamBlizzard <dan.sabo@live.ca>
2024-10-02 16:30:48 -0600

    Add python time conversion utility; implement classic thriller tune.

--- 1_music.s
+++ 1_music.s
@@ -1,22 +1,38 @@
     processor 6502
 
 ; KERNAL [sic] address
-CHROUT  = $ffd2
-A       = $030c
-X       = $030d
-Y       = $030e
+CHROUT  = $FFD2
 
 ; SPEAKER addresses
-VOL     = $900e   ; volume
-SPKR1   = $900a   ; low frequency
-SPKR2   = $900b   ; mid frequency
-SPKR3   = $900c  ; high frequency
-SPKR4   = $900d   ; noise
+VOL     = $900E     ; volume
+SPKR1   = $900A     ; low frequency
+SPKR2   = $900B     ; mid frequency
+SPKR3   = $900C     ; high frequency
+SPKR4   = $900D     ; noise
 
-; MUSIC TIMER addresses
-TIMER1  = $1DFD  ; 256^1
-TIMER2  = $1DFE   ; 256^2
-TIMER3  = $1DFF   ; 256^3
+; JIFFY CLOCK addresses (resets every 24 hours)
+JIFFY3  = $00A0     ; n * 256^0
+JIFFY2  = $00A1     ; n * 256^1
+JIFFY1  = $00A2     ; n * 256^2
+
+; MUSIC TIMER addresses (jiffy with manual reset)
+LASTJIFFY = $1DFC   ; stores last known jiffy
+TIMER1  = $1DFF     ; n * 256^0
+TIMER2  = $1DFE     ; n * 256^1
+TIMER3  = $1DFD     ; n * 256^2
+
+TIMERESET1  = #168  ; 168 * 256^0 = 168 jiffies
+TIMERESET2  = #0    ; 0 * 256^1 = 0 jiffies
+TIMERESET3  = #0    ; 0 * 256^2 = 0 jiffies
+
+PLAYNOTE1 = #168
+STOPNOTE1 = #162
+
+PLAYNOTE2 = #84
+STOPNOTE2 = #78
+
+PLAYNOTE3 = #74
+STOPNOTE3 = #68
 
 ; -------- NOTES --------
 A1      = #183
@@ -83,33 +99,83 @@ nextstmt                ; next BASIC statement
     lda #5          ; load volume as 10
     sta VOL         ; store into volume register (AKA set volume)
 
-    lda #255        ; load timer 1 as 255
+    lda JIFFY1      ; load 0 for jiffy
+    sta LASTJIFFY   ; store last known jiffy
+
+    lda #TIMERESET1 ; load timer 1 as 255
     sta TIMER1      ; store into timer 1 register
 
-    lda #255        ; load timer 1 as 255
+    lda #TIMERESET2 ; load timer 1 as 255
     sta TIMER2      ; store into timer 2 register
 
-    lda #2          ; load timer 3 as 31
+    lda #TIMERESET3 ; load timer 3 as 31
     sta TIMER3      ; store into timer 3 register
 
-    lda #Fs1        ; load x register as F#1
-    sta SPKR3       ; store into high frequency register
+    lda #F1         ; load register as F
+    sta SPKR2       ; store into high frequency register
 
+    ; print out "The Shape Stalks"
+    lda #'T
+    jsr CHROUT
+    lda #'H
+    jsr CHROUT
+    lda #'E
+    jsr CHROUT
+    lda #32
+    jsr CHROUT
+    lda #'S
+    jsr CHROUT
+    lda #'H
+    jsr CHROUT
+    lda #'A
+    jsr CHROUT
+    lda #'P
+    jsr CHROUT
+    lda #'E
+    jsr CHROUT
+    lda #32
+    jsr CHROUT
+    lda #'S
+    jsr CHROUT
+    lda #'T
+    jsr CHROUT
+    lda #'A
+    jsr CHROUT
+    lda #'L
+    jsr CHROUT
+    lda #'K
+    jsr CHROUT
+    lda #'S
+    jsr CHROUT
+
+    ; Main loop
 gameloop:           ; play a note on loop
-    lda TIMER3      ; check if timer 3 is 0
-    beq volume_on   ; if so, turn volume on
-    sbc #1         ; check if timer 3 is 16
-    beq volume_off  ; if so, turn volume off
-    jmp next_loop   ; otherwise, continue loop
+    lda TIMER1
+    cmp #PLAYNOTE1
+    beq volume_on
+    cmp #PLAYNOTE2
+    beq volume_on
+    cmp #PLAYNOTE3
+    beq volume_on
 
+    cmp #STOPNOTE1
+    beq volume_off
+    cmp #STOPNOTE2
+    beq volume_off
+    cmp #STOPNOTE3
+    beq volume_off
+    lda #0
+    beq next_loop
+    
 volume_off:
-    lda #0          ; set volume to 0
-    sta VOL         ; store into volume register
-    jmp next_loop   ; continue loop
+    lda #0
+    sta VOL
+    beq next_loop
 
-volume_on:          ; play note
-    lda #5          ; set volume to 5
-    sta VOL         ; store into volume register
+volume_on:
+    lda #5
+    sta VOL
+
 
 next_loop:
     jsr increment_timer ; increment the timer
@@ -121,17 +187,22 @@ end:                    ; end of program
 ; -------- END PROGRAM --------
 
 
-
 ; -------- SUBROUTINES --------
 
 ; increment_timer
     ; decrements the timers and loops them
     ; allows for approx. 16 seconds of looped data, such as music
 increment_timer:
-    lda TIMER1              ; check if timer 1 is 0
+    lda JIFFY1              ; load current jiffy
+    cmp LASTJIFFY           ; compare to last jiffy
+    bne increment_timer1    ; if jiffy elapsed, update timer
+    jmp check_timer         ; otherwise, return
+    
+increment_timer1:
+    sta LASTJIFFY           ; store current jiffy
     beq increment_timer2    ; if so, increment timer 2
     dec TIMER1              ; otherwise, decrement timer 1
-    rts                     ; return from subroutine
+    jmp check_timer         ; return from subroutine
 
 increment_timer2:
     lda #255                ; reset timer 1
@@ -139,7 +210,7 @@ increment_timer2:
     lda TIMER2              ; check if timer 2 is 0
     beq increment_timer3    ; if so, increment timer 3
     dec TIMER2              ; otherwise, decrement timer 2
-    rts                     ; return from subroutine
+    jmp check_timer         ; return from subroutine
 
 increment_timer3:
     lda #255                ; reset timer 2
@@ -147,11 +218,31 @@ increment_timer3:
     lda TIMER3              ; check if timer 3 is 0
     beq loop_timer          ; if so, loop timer
     dec TIMER3              ; otherwise, decrement timer 3
-    rts                     ; return from subroutine
+    jmp check_timer         ; return from subroutine
 
 loop_timer:
-    lda #2                 ; reset timer 3
+    lda #255                ; reset timer 3
     sta TIMER3              ; store into timer 3 register
-    rts                     ; return from subroutine
+
+check_timer:
+    sta LASTJIFFY           ; store current jiffy
+    lda TIMER1              ; check if timer 1 is 0
+    bne end_timer           ; if so, increment timer 1
+    lda TIMER2              ; check if timer 2 is 0
+    bne end_timer           ; if so, increment timer 2
+    lda TIMER3              ; check if timer 3 is 0
+    bne end_timer           ; if so, increment timer 3
+
+    lda #TIMERESET1     ; 168 * 256^0 = 168 jiffies
+    sta TIMER1          ; store the timer value in address TIMER1
+
+    lda #TIMERESET2     ; 0 * 256^1 = 0 jiffies
+    sta TIMER2          ; store the timer value in address TIMER2
+
+    lda #TIMERESET3     ; 0 * 256^2 = 0 jiffies
+    sta TIMER3          ; store the timer value in address TIMER3
+
+end_timer:
+    rts                 ; return from subroutine
 
 ; END increment_timer

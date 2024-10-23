e3ab0950a34c36879dd4fc378e6cdd03937124d5
Dante <dkirsman@gmail.com>
2024-10-23 17:41:19 -0600

    fix(addresses for SRC and DST): the code still doesnt work tho

--- a2-compression/zx02-optim.s
+++ a2-compression/zx02-optim.s
@@ -25,29 +25,33 @@ nextstmt
   incbin "titlescreen.zx02"
 
 ZP      = $00          
-offset  = ZP+0
-ZX0_src = ZP+2
-ZX0_dst = ZP+4
-bitr    = ZP+6
-pntr    = ZP+7
+OFFSET  = ZP+0
+ZX0_SRC = ZP+2
+ZX0_DST = ZP+4
+BITR    = ZP+6
+PNTR    = ZP+7
 
 
 zx0_ini_block:
   lda #$00                ; load offset
-  sta $00                 ; offset = 0
-  sta $01                 ; offset = 0
-  lda #$0D                ; load compiled data, lower byte
-  sta $02
-  lda #$10                ; load compiled data, uopper byte
-  sta $03
-  lda #$00                ; set destination location lower byte
-  sta $04
-  lda #$1E                ; set destination location upper byte
-  sta $05         
-  lda #$80                ; bitr = $80
-  sta $06
-  lda #$ff                ; pntr = $ff              
-  sta $07
+  sta OFFSET              ; offset = 0
+  sta OFFSET+1            ; offset = 0
+
+  ; Load the compiled data location (It's 100D according to memory editor in the emulator)
+  lda #$0D                ; lower byte
+  sta ZX0_SRC
+  lda #$10                ; upper byte
+  sta ZX0_SRC+1
+
+  ; Set the data destination location (1E00 is the screen memory)
+  lda #$00                ; lower byte
+  sta ZX0_DST
+  lda #$1E                ; upper byte
+  sta ZX0_DST+1
+  
+  ; Set BITR
+  lda #$80                ; BITR = $80
+  sta BITR
 
 ;--------------------------------------------------
 ; Decompress ZX0 data (6502 optimized format)
@@ -62,52 +66,52 @@ decode_literal:
   jsr get_elias
 
 cop0:
-  lda (ZX0_src),y
-  inc ZX0_src
+  lda (ZX0_SRC),y
+  inc ZX0_SRC
   bne cop0_dst
-  inc ZX0_src+1
+  inc ZX0_SRC+1
 
 cop0_dst:   
-  sta (ZX0_dst),y
-  inc ZX0_dst
+  sta (ZX0_DST),y
+  inc ZX0_DST
   bne cop0_offset
-  inc ZX0_dst+1
+  inc ZX0_DST+1
 
 cop0_offset:
   dex
   bne cop0
 
-  asl bitr
+  asl BITR
   bcs dzx0s_new_offset
 
 ; Copy from last offset (repeat N bytes from last offset)
 ;    Elias(length)
   jsr get_elias
 dzx0s_copy:
-  lda ZX0_dst
-  sbc offset  ; C=0 from get_elias
-  sta pntr
-  lda ZX0_dst+1
-  sbc offset+1
-  sta pntr+1
+  lda ZX0_DST
+  sbc OFFSET  ; C=0 from get_elias
+  sta PNTR
+  lda ZX0_DST+1
+  sbc OFFSET+1
+  sta PNTR+1
 
 cop1:
-  lda (pntr),y
-  inc pntr
+  lda (PNTR),y
+  inc PNTR
   bne cop1_dst
-  inc pntr+1
+  inc PNTR+1
 
 cop1_dst:
-  sta (ZX0_dst),y
-  inc ZX0_dst
+  sta (ZX0_DST),y
+  inc ZX0_DST
   bne cop1_decode
-  inc ZX0_dst+1
+  inc ZX0_DST+1
 
 cop1_decode:
   dex
   bne cop1
 
-  asl bitr
+  asl BITR
   bcc decode_literal
 
 ; Copy from new offset (repeat N bytes from new offset)
@@ -120,18 +124,18 @@ dzx0s_new_offset:
   dex
   txa
   lsr
-  sta offset+1
+  sta OFFSET+1
 
   ; Get low part of offset, a literal 7 bits
-  lda (ZX0_src),y
-  inc ZX0_src
+  lda (ZX0_SRC),y
+  inc ZX0_SRC
   bne dzx0s_divide
-  inc ZX0_src+1
+  inc ZX0_SRC+1
 
 dzx0s_divide:
   ; Divide by 2
   ror
-  sta offset
+  sta OFFSET
 
   ; And get the copy length.
   ; Start elias reading with the bit already in carry:
@@ -144,24 +148,24 @@ dzx0s_divide:
 ; Read an elias-gamma interlaced code.
 ; ------------------------------------
 elias_get:     ; Read next data bit to result
-  asl bitr
+  asl BITR
   rol
   tax
 
 get_elias:
   ; Get one bit
-  asl bitr
+  asl BITR
   bne elias_skip1
 
   ; Read new bit from stream
-  lda (ZX0_src),y
-  inc ZX0_src
+  lda (ZX0_SRC),y
+  inc ZX0_SRC
   bne elias_bitr
-  inc ZX0_src+1
+  inc ZX0_SRC+1
 
 elias_bitr:
   rol
-  sta bitr
+  sta BITR
 
 elias_skip1:
   txa

70f94346abff6aa10d44d76e121c32d0f24658f8
Dante <dkirsman@gmail.com>
2024-10-27 20:42:30 -0600

    feat(exo works): AAAAAHHHHHHHHHHHHHHHHHH IT WORKEDDDDDDDDDDDDDDD

--- a2-compression/exo-decomp.s
+++ a2-compression/exo-decomp.s
@@ -1,18 +1,47 @@
+CHROUT = $FFD2
+SCREEN = $900F
+PLOT   = $FFF0
+
 ; BASIC stub
   processor 6502
   org $1001
     dc.w nextstmt       
     dc.w 10             
-    dc.b $9e, "4109", 0 
+    dc.b $9e, [start]d, 0 
 nextstmt               
     dc.w 0              
-
 
   incbin "titlescreen.exo"
 exo_data_end
 
-exomizer_decomp:
+start:
+  lda #147              ; Load clear screen command
+  jsr CHROUT            ; Print it
+
+bg:
+  ldx #0              ; Set X to black
+  lda SCREEN          ; Load the screen colour address
+  and	#$0F            ; Reset the 4 background bits
+  stx $1001           ; Store X into the user basic area memory
+  ora $1001           ; Combine new background color with the screen
+  sta SCREEN          ; Store new colour into the screen colour address
+
+
+border:
+  ldx #0              ; Set X to black
+  lda SCREEN          ; Load the screen colour address
+  and	#$F8            ; Reset the 3 border bits
+  stx $1001           ; Store X into the user basic area memory
+  ora $1001           ; Combine new border colour with the screen 
+  sta SCREEN          ; Store new colour into the screen colour address
+
+title:
+  ldx #1              ; Set X to white
+  stx $0286           ; Store X into current color code address
+
+decrunch:
   jsr exod_decrunch
+  jmp loop
 
 ; We need this function
 ; I stole it from the main.s in the exomizer repo

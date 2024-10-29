86509128c298247fea1a2df5d41b335fb9ac03f5
Dante <dkirsman@gmail.com>
2024-10-28 23:06:59 -0600

    docs(comments): add/fix comments

--- a2-compression/rle.s
+++ a2-compression/rle.s
@@ -1,5 +1,4 @@
 CHROUT = $FFD2
-PLOT   = $FFF0
 SCREEN = $900F
 
 ; BASIC stub
@@ -13,91 +12,89 @@ nextstmt
 
 OFFSET = $00
 COMPRESSED_CHAR = $01
-RUN_LENGTH_TEST = $02
-DATA_SECTION = $03  ; New flag to indicate the current RLE data section
+DATA_SECTION = $02  
 
 ; Start of program
 clr:
-  lda #147              ; Load clear screen command
-  jsr CHROUT            ; Print it
+  lda #147                ; Load clear screen command
+  jsr CHROUT              ; Print it
 
 bg:
-  ldx #0                ; Set X to black
-  lda SCREEN            ; Load the screen colour address
-  and #$0F              ; Reset the 4 background bits
-  stx $1001             ; Store X into the user basic area memory
-  ora $1001             ; Combine new background color with the screen
-  sta SCREEN            ; Store new colour into the screen colour address
+  ldx #0                  ; Set X to black
+  lda SCREEN              ; Load the screen colour address
+  and #$0F                ; Reset the 4 background bits
+  stx $1001               ; Store X into the user basic area memory
+  ora $1001               ; Combine new background color with the screen
+  sta SCREEN              ; Store new colour into the screen colour address
 
 border:
-  ldx #0                ; Set X to black
-  lda SCREEN            ; Load the screen colour address
-  and #$F8              ; Reset the 3 border bits
-  stx $1001             ; Store X into the user basic area memory
-  ora $1001             ; Combine new border colour with the screen 
-  sta SCREEN            ; Store new colour into the screen colour address
+  ldx #0                  ; Set X to black
+  lda SCREEN              ; Load the screen colour address
+  and #$F8                ; Reset the 3 border bits
+  stx $1001               ; Store X into the user basic area memory
+  ora $1001               ; Combine new border colour with the screen 
+  sta SCREEN              ; Store new colour into the screen colour address
 
 title:
-  ldx #1                ; Set x to white
-  stx $0286             ; Store x into current color code address
+  ldx #1                  ; Set x to white
+  stx $0286               ; Store x into current color code address
 
 decompress:
-  lda #0                ; Reset a
-  tay                   ; Set y to 0              
-  sta OFFSET            ; Set offset to 0
-  sta DATA_SECTION      ; Set DATA_SECTION to 0
+  lda #0                  ; Reset a
+  tay                     ; Set y to 0              
+  sta OFFSET              ; Set offset to 0
+  sta DATA_SECTION        ; Set DATA_SECTION to 0
 
 next_char:
-  ldx OFFSET            ; Grab the offset 
-  lda DATA_SECTION
-  beq load_title_data   ; If DATA_SECTION is 0, use rle_title_data
-  lda rle_bank_data,x   ; Otherwise, use rle_bank_data
+  ldx OFFSET              ; Grab the offset 
+  lda DATA_SECTION        ; Check which section we are decompressing
+  beq load_title_data 
+  lda rle_bank_data,x     ; Load the current character to decompress for title
   jmp process_char
 
 load_title_data:
-  lda rle_title_data,x
+  lda rle_title_data,x    ; Load the current character to decompress for title
 
 process_char:
-  sta COMPRESSED_CHAR   ; Store the char for later use
-  beq check_data_section ; If character is zero, check data section
+  sta COMPRESSED_CHAR     ; Store the char for later use
+  beq check_data_section  ; If character is zero, check data section
 
-  inx                   ; Move to the next RLE data byte
-  stx OFFSET            ; Set the new offset for later
-  lda DATA_SECTION
-  beq load_title_length
-  lda rle_bank_data,x
+  inx                     ; Move to the next RLE data byte
+  stx OFFSET              ; Set the new offset for later
+  lda DATA_SECTION        ; Check which section we are decompressing
+  beq load_title_length    
+  lda rle_bank_data,x     ; Load the run length value for bank
   jmp store_run_length
 
 load_title_length:
-  lda rle_title_data,x
+  lda rle_title_data,x    ; load the run length value for title
 
 store_run_length:
-  sta RUN_LENGTH_TEST
-  tax
+  tax                     ; Store the run length in X
 
 decompress_char:
-  lda COMPRESSED_CHAR   ; Load the character
-  jsr CHROUT            ; Print to screen
-  dex                   ; Decrement run length
-  bne decompress_char   ; Repeat until run length is zero
+  lda COMPRESSED_CHAR     ; Load the character
+  jsr CHROUT              ; Print to screen
+  dex                     ; Decrement run length
+  bne decompress_char     ; Repeat until run length is zero
 
-  inc OFFSET            ; Increment offset since we finished the run length
-  jmp next_char         ; Go to the next character
+  inc OFFSET              ; Increment offset since we finished the run length
+  jmp next_char           ; Go to the next character
 
 check_data_section:
   lda DATA_SECTION
-  beq switch_to_bank    ; If we are in the title section, switch to bank data
-  jmp end               ; Otherwise, we are done
+  beq switch_to_bank      ; If we are in the title section, switch to bank data
+  jmp end                 ; Otherwise, we are done
 
 switch_to_bank:
   lda #1
-  sta DATA_SECTION      ; Set DATA_SECTION to 1 for rle_bank_data
+  sta DATA_SECTION        ; Set DATA_SECTION to 1 for rle_bank_data
   lda #0
-  sta OFFSET            ; Reset OFFSET to 0 for new data section
-  jmp next_char         ; Start decompressing rle_bank_data
+  sta OFFSET              ; Reset OFFSET to 0 for new data section
+  jmp next_char           ; Start decompressing rle_bank_data
 
 end:
-  jmp end               ; Infinite loop at the end
+  jmp end                 ; Infinite loop
 
 ; RLE Data Storage
 ; THE BIG APPLE
@@ -116,6 +113,7 @@ rle_title_data:
   .byte #$20, #$07, #$4a, #$01, #$49, #$01, #$4e, #$01, #$20, #$02, #$53, #$01, #$4f, #$01, #$4e, #$01, #$47, #$01, #$20, #$06
   .byte #$20, #$16
   .byte #$20, #$09, #$32, #$01, #$30, #$01, #$32, #$01, #$34, #$01, #$20, #$09
+  .byte #$20, #$16
   .byte #$20, #$16
   ; End marker for title data
   .byte #$00

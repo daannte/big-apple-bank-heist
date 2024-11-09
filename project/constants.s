2b5545f3922a108fce444c6683ed048193ac038a
SteamBlizzard <dan.sabo@live.ca>
2024-11-09 14:18:39 -0700

    Separate constants into includable file

---
+++ project/constants.s
@@ -0,0 +1,35 @@
+TIMERESET1  = #3
+TIMERESET2  = #0
+TIMERESET3  = #0
+
+; CHARACTER codes
+ROBBER_R    = #64
+ROBBER_R_1  = #65
+ROBBER_R_2  = #66
+
+ROBBER_L    = #67
+ROBBER_L_1  = #68
+ROBBER_L_2  = #69
+
+ROBBER_VL_1 = #70
+ROBBER_VL_2 = #71
+ROBBER_VR_1 = #72
+ROBBER_VR_2 = #73
+
+WALL        = #74
+EXITDOOR    = #75
+
+ASCII_0     = #76
+ASCII_1     = #77
+ASCII_2     = #78
+ASCII_3     = #79
+ASCII_4     = #80
+ASCII_5     = #81
+ASCII_6     = #82
+ASCII_7     = #83
+ASCII_8     = #84
+ASCII_9     = #85
+
+EMPTY_SPACE_CHAR  = #86
+HEART_CHAR        = #87
+TIMER_MAX_VALUE   = #15
--- project/main.s
+++ project/main.s
@@ -14,46 +14,10 @@ JIFFY1  = $00A2     ; n * 256^2
 JIFFY2  = $00A1     ; n * 256^1
 JIFFY3  = $00A0     ; n * 256^0
 
-TIMERESET1  = #3
-TIMERESET2  = #0
-TIMERESET3  = #0
-
-; CHARACTER codes
-ROBBER_R    = #64
-ROBBER_R_1  = #65
-ROBBER_R_2  = #66
-
-ROBBER_L    = #67
-ROBBER_L_1  = #68
-ROBBER_L_2  = #69
-
-ROBBER_VL_1 = #70
-ROBBER_VL_2 = #71
-ROBBER_VR_1 = #72
-ROBBER_VR_2 = #73
-
-WALL        = #74
-EXITDOOR    = #75
-
-ASCII_0     = #76
-ASCII_1     = #77
-ASCII_2     = #78
-ASCII_3     = #79
-ASCII_4     = #80
-ASCII_5     = #81
-ASCII_6     = #82
-ASCII_7     = #83
-ASCII_8     = #84
-ASCII_9     = #85
-
-EMPTY_SPACE_CHAR  = #86
-HEART_CHAR        = #87
-TIMER_MAX_VALUE   = #15
-
-  include "zeropage.s"
-
-  org $1001
   incdir "project"
+  include "constants.s"
+  include "zeropage.s"
+  org $1001
   include "stub.s"
   
 comp_data
@@ -62,6 +26,7 @@ comp_data
 level1_data
   incbin "level1.data"
 
+; 00100101
 timer_value:
     .BYTE #69         ; Nice
 
